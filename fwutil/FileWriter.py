7c35589cddccc089e406db7d161c0cbba615cc1f
1fexd <PGFvy85nC@protonmail.com>
2023-07-14 21:13:53 +0200

    feat: Add new line util method

--- fwutil/FileWriter.py
+++ fwutil/FileWriter.py
@@ -6,9 +6,13 @@ class FileWriter:
     def __init__(self, file: TextIO):
         self.file = file
 
-    def write_new_line(self, text: str, indent="", clean=False):
+    def write_text_new_line(self, text: str, indent="", clean=False):
         self.write(text, indent, clean)
-        self.file.write("\n")
+        self.write_new_line(1)
+
+    def write_new_line(self, amount: int = 1):
+        for i in range(0, amount):
+            self.file.write("\n")
 
     def write(self, text: str, indent="", strip_first_newline=True, clean=False):
         if clean:
