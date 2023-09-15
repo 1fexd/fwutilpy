68e4e51d17d6beb29ba5bdb0feae17eba9a2760b
1fexd <PGFvy85nC@protonmail.com>
2023-09-15 15:39:41 +0200

    feat: Add list writing capabilities

--- fwutil/FileWriter.py
+++ fwutil/FileWriter.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import contextlib
 from textwrap import dedent
 from types import TracebackType
-from typing import TextIO, Optional, Type
+from typing import TextIO, Optional, Type, TypeVar, Callable
 
 from fwutil.WriteMode import WriteMode, open_text_write_file
 
@@ -34,7 +34,8 @@ class FileWriter(contextlib.AbstractContextManager):
         for i in range(0, amount):
             self._write(self.__NEW_LINE)
 
-    def write_multiline(self, text: str, indent="", strip_first_newline=True, clean=True, auto_newline=True):
+    def write_multiline(self, text: str, indent: str = "", strip_first_newline: bool = True, clean: bool = True,
+                        auto_newline: bool = True):
         if auto_newline and self.__has_buffered_lines():
             self.new_line()
 
@@ -46,6 +47,18 @@ class FileWriter(contextlib.AbstractContextManager):
             text = self.__NEW_LINE.join([indent + line for line in lines])
 
         self._write(text)
+
+    def write_list(self, text_list: [str], separator: str = ", ", indent: str = "", new_lines: int = 0, clean=False):
+        self.write_line(separator.join(text_list), indent, new_lines, clean)
+
+    T = TypeVar("T")
+
+    def transform_and_write_list(self, items: [T], separator: str = ", ",
+                                 transform: Callable[[T], str] = lambda val: str(val),
+                                 indent: str = "",
+                                 new_lines: int = 0,
+                                 clean=False):
+        return self.write_list([transform(item) for item in items], separator, indent, new_lines, clean)
 
     def _write(self, text: str):
         self._file.write(text)
